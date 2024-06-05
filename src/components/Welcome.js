821e8af6fc398866203eaad027a972add355de2a
madina <mad80ina@gmail.com>
2024-06-05 15:07:16 +0500

    add files

---
+++ src/components/Welcome.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div className="component">
+        <h1>Class Component</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+          distinctio!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
